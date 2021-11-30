f4e66907fda6d7b85078f7eba0d95e5155597d60
td236 <dadela.tomasz@gmail.com>
2021-11-30 13:48:13 +0100

    feat: run terminal

---
+++ wm.py
@@ -0,0 +1,22 @@
+import Xlib
+import subprocess
+
+
+class wm():
+    pass
+
+
+def main():
+    process = subprocess.Popen(['echo', 'More output'])
+    # subprocess.Popen(["/usr/bin/feh", " --bg-fill", " /home/td/project/Todo-background-system/img/bg.jpg"])
+    subprocess.Popen(r"/usr/bin/xterm")
+    print("Test")
+    def __init__():
+        subprocess.Popen(r"/usr/bin/feh --bg-fill /home/td/project/Todo-background-system/img/bg.jpg")
+        subprocess.Popen(r"/usr/bin/xfce4-terminal")
+        print("Test init")
+
+WindowManager = wm()
+
+if __name__ == "__main__":
+    main()
