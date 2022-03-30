282cec0b0df64eb99c45688fda619e2445a0c973
Jakub Parapura <jakubparapura@gmail.com>
2022-03-30 19:48:01 +0200

    feat: status bar - draft

--- lib/workspace_manager.py
+++ lib/workspace_manager.py
@@ -1,4 +1,5 @@
 from lib.node import Node
+import socket
 
 
 class WorkspaceManager:
@@ -7,6 +8,11 @@ class WorkspaceManager:
         self.workspaces = [
             Node(geometry, None, None) for i in range(no_workspaces)]
         self.active_workspace = 0
+
+        # refactor
+        self.host = socket.gethostname()
+        self.port = 8080
+
 
     def get_current_workspace(self):
         return self.workspaces[self.active_workspace]
@@ -22,6 +28,12 @@ class WorkspaceManager:
 
         for window in self.get_current_workspace().windows:
             window.map()
+
+        message = str(target + 1)
+        breakpoint()
+        with socket.socket() as s:
+            s.connect((self.host, self.port))
+            s.send(message.encode('utf-8'))
 
         # self.draw_windows()
 
