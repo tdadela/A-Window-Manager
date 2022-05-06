7d082893175da02a28908e2107c59acad0264422
td236 <dadela.tomasz@gmail.com>
2022-05-06 12:12:35 +0200

    tests: vertical mode

--- tests/tiling.py
+++ tests/tiling.py
@@ -2,7 +2,7 @@ from tests.mock.mock_window import mock_window
 from awm.distribute_windows import distribute_windows
 
 
-def zero_windows():
+def zero_windows(is_horizontal):
     '''Case: work space without any window.'''
     screen_width = 1920
     screen_height = 1080
@@ -11,11 +11,11 @@ def zero_windows():
         windows,
         width=screen_width,
         height=screen_height,
-        horizontal=True)
+        horizontal=is_horizontal)
     assert sum([win.get_area() for win in windows]) == 0
 
 
-def n_windows():
+def n_windows(is_horizontal):
     '''Case: work space with 1 to 5 windows.'''
     screen_width = 1920
     screen_height = 1080
@@ -25,12 +25,14 @@ def n_windows():
             windows,
             width=screen_width,
             height=screen_height,
-            horizontal=True)
+            horizontal=is_horizontal)
 
         assert sum([win.get_area() for win in windows]
                    ) == screen_height * screen_width
 
 
 if __name__ == '__main__':
-    zero_windows()
-    n_windows()
+    zero_windows(True)
+    n_windows(True)
+    zero_windows(False)
+    n_windows(False)
